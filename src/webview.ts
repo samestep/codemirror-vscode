549b0eca731e04a7f77e55ca46a5e001103f7df2
Sam Estep <sam@samestep.com>
2025-03-25 23:03:50 -0400

    Use a better theme

--- src/webview.ts
+++ src/webview.ts
@@ -1,8 +1,9 @@
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { basicSetup } from "codemirror";
 import { sync } from "./sync-webview";
 
 sync({
   vscode: acquireVsCodeApi(),
-  extensions: [basicSetup],
+  extensions: [basicSetup, vscodeDark],
   parent: document.getElementById("editor")!,
 });
